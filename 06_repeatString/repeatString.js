eb3b60cc94dd34d1b39640d948fd80cd72a08cfa
Dotty <45690879+AxelDots@users.noreply.github.com>
2025-09-22 04:19:11 +0300

    finished the task

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(word, amount) {
+    for (var i = 0; i < amount; i++) {
+        console.log(word);
+    }
 };
 
 // Do not edit below this line
